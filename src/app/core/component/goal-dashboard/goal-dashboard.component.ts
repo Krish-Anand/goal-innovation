4dfb0a7f5168e0fc9de7a1d56fd9167076718890
K V <karthik.k.v@accenture.com>
2022-02-16 13:01:14 +0530

    Redesigned Application

--- src/app/core/component/goal-dashboard/goal-dashboard.component.ts
+++ src/app/core/component/goal-dashboard/goal-dashboard.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import{ HttpClient } from '@angular/common/http'
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-goal-dashboard',
   templateUrl: './goal-dashboard.component.html',
   styleUrls: ['./goal-dashboard.component.scss']
 })
+
+
 export class GoalDashboardComponent implements OnInit {
 
-  constructor() { }
+//  url!: string;
+//  goalDetails : GoalDetails | undefined;
+
+
+
+  constructor() { 
+
+
+    // // this.url = route.url.split('/')[0];
+
+    // console.log(route.url)
+
+  }
 
   ngOnInit(): void {
   }
